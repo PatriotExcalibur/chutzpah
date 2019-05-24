3bcfd8a238659d77a21ea4131c445ddfb4acc432
Scott McNally <scott.mcnally@pexmain.com>
2019-05-24 15:22:57 -0500

    4.4.6.1 versioning

--- Facts.Integration/Properties/AssemblyInfo.cs
+++ Facts.Integration/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using Xunit;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.3.7.2")]
+// [assembly: AssemblyVersion("4.4.6.1")]
 [assembly: AssemblyVersion("4.4.6.1")]
 [assembly: AssemblyFileVersion("4.4.6.1")]
 
